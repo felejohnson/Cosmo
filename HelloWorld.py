f58854ec85ed02ab7c420ec364c7636954408e29
Felicia <feliciaj61@gmail.com>
2017-02-07 20:15:03 -0500

    meh

---
+++ HelloWorld.py
@@ -0,0 +1,65 @@
+'''print("Hello World")
+print ("Goodnight Moon")
+"There is cake in the hallway" \
+ The i scake in the hallway
+
+a="0"
+b="2"
+print (a+b)
+
+a=2
+b=3
+print (a+b)
+c="9"
+d=2
+print(c+d)
+
+
+a=2
+b=3
+print(a+b)
+c="9"
+d=2
+print(c+d)
+
+Test1=float(input ("Please Input the score for Test 1"))
+Test2=float(input ("Please Input the score for Test 2"))
+Test3=float(input ("Please Input the score for Test 3"))
+average= (Test1+Test2+Test3)/3
+print("The Average of all 3 tests is",average)
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(3%2)
+print(3**4)
+
+a=0
+a+=2
+ (a)
+
+print (5.0/9.0)
+print (5.0/9)
+print (5/9.0)
+print (5/9)
+print (9.0/5.0)
+print (9.0/5)
+print (9/5)'''
+
+temp= int(input ("Enter the temperature: "))
+if temp >60 and temp<90:
+    print ("warm")
+elif temp >=90:
+    print ("hot")
+elif temp <60:
+    print ("chilly")
+
+
+
+
+
+
+
+
+
